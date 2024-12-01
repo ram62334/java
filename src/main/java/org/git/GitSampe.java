25c9c119e65e5d4d5178f527dcb2266fe1f55628
ram62334 <76649111+ram62334@users.noreply.github.com>
2024-12-01 16:30:54 +0530

    first commit

--- src/main/java/org/git/GitSampe.java
+++ src/main/java/org/git/GitSampe.java
@@ -2,6 +2,6 @@ package org.git;
 
 public class GitSampe {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git, SMALL CHANGEGIT");
     }
 }
