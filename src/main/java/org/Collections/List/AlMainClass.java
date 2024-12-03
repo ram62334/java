4586c08006a785b8eeb9344f7c549fb0ae98b5a4
ram62334 <76649111+ram62334@users.noreply.github.com>
2024-12-03 14:03:13 +0530

    java 8(Predicate, function, consumer)

--- src/main/java/org/Collections/List/AlMainClass.java
+++ src/main/java/org/Collections/List/AlMainClass.java
@@ -1,10 +1,9 @@
 package org.Collections.List;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public class AlMainClass implements DemoFace, DemoFace2{
 
@@ -60,13 +59,61 @@ public class AlMainClass implements DemoFace, DemoFace2{
             }
         }
 //        combining two predicates
-//        predicate to check if names start with 'r'
+//        predicate to check if names start with 'r' and length>3
         Predicate<String> p2 = s1 -> s1.charAt(0) == 'r';
         for (String st : str) {
 //      we can use 'AND' 'OR','NEGATE  interchangebly
 
             if (p1.and(p2).test(st))
                 System.out.println("names having length>3 and starts with 'r'" + st);
+        }
+
+//        Function
+//        Predicate only returns boolean type you want to return different types? use Function
+//        it is a FI, it's methos is apply()
+        List<Student> sl = new ArrayList();
+        sl.add(new Student("p11", "ram", 85));
+        sl.add(new Student("p12", "ravi", 65));
+        sl.add(new Student("p13", "shyam", 75));
+        sl.add(new Student("p14", "sunny", 90));
+        sl.add(new Student("p15", "bunny", 40));
+
+//        generate grades using Function
+        Function<Student,String> func = s->{
+            String grade="";
+            int m = s.marks;
+            if (m>85) grade = "DST";
+                    else if (m>=60) grade = "FC";
+                    else if (m>=50) grade = "SC";
+                    else grade = "F";
+                    return grade;
+        };
+//        Function chainng
+        Function<Integer, Integer> f1 = i->2*i;
+        Function<Integer, Integer> f2 = i->i*i*i;
+//        below f1 is executed first follwed by next
+        System.out.println(f1.andThen(f2).apply(4));//4
+//        below f2 folled by f1
+        System.out.println(f1.compose(f2).apply(2));//i*i*i->8->2*8->16
+
+//      Predicate to check if student marks more than 60
+        Predicate<Student> p = s -> s.marks>60;
+
+//      CONSUMER
+//        It is a FI
+//        it does not return anything but only consumes, it's method is accept
+//        consumer to display student details
+        Consumer<Student> c = s->{
+            System.out.println("Student id "+s.id);
+            System.out.println("Student name "+s.name);
+            System.out.println("Student marks "+s.marks);
+            System.out.println("");
+        };
+
+        for (Student s:sl){
+//         fetch student details if marks>60 or grade is FC
+            if (func.apply(s).equals("FC") || p.test(s))
+                c.accept(s);
         }
     }
 }
---
+++ src/main/java/org/Collections/List/Student.java
@@ -0,0 +1,13 @@
+package org.Collections.List;
+
+public class Student {
+    String id;
+    String name;
+    int marks;
+
+    Student(String id, String name, int marks){
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+}
