1c69a94a904ab8ccea73d461caee472031761616
ram62334 <76649111+ram62334@users.noreply.github.com>
2024-12-06 16:00:02 +0530

    this is push commit

---
+++ Git/GitSample.java
@@ -0,0 +1,11 @@
+package org.Git;
+
+public class GitSample {
+    public static void main(String[] args) {
+        System.out.println("new node");
+
+        System.out.println("check for new node");
+        System.out.println("welcome to git");
+        System.out.println("git push");
+    }
+}
